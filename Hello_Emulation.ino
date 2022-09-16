bb542af917e51ab83a754cb71f842e84b154a4dc
Abel Wondafrash <abel.wondafrash@gmail.com>
2022-09-17 01:14:20 +0300

    Create Hello_Emulation.ino
    * Requires Serial Port for Serial Communication
    * Baud rate & LCD size should match with Processing side
    * Everything else is the same -- Use LCDemulator as you would LiquidCrystal.h or its I2C version

---
+++ Hello_Emulation.ino
@@ -0,0 +1,18 @@
+#include "LCDemulator.h"
+LCDemulator lcd;
+
+void setup() {
+  Serial.begin (9600); // Required by LCD Emulator for Serial Communication
+                       // Must match that of LCD Emulator Processing Side
+  
+  lcd.begin (20, 4); // Set the Character LCD size matching the Processing side here
+  lcd.setCursor (0, 1);
+  lcd.print ("Emulated");
+  
+  delay (1000);
+  
+  lcd.clear ();
+}
+
+void loop() {
+}
